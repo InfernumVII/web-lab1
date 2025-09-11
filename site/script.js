24cb1ff1b5c1875230e05804e3ac7c6ef8617438
InfenrumVII <egor554shved@gmail.com>
2025-09-11 04:11:56 +0300

    kinda complete front

---
+++ site/script.js
@@ -0,0 +1,48 @@
+const numberRegex = new RegExp("^-?\\d+$")
+function checkDecimal(text){
+    return numberRegex.test(text)
+}
+function checkNumberBetween(a, b, number){
+    return number >= -5 && number <= 3
+}
+function checkValueWithError(text){
+    let errorText = ""
+    let success = false
+    if (!checkDecimal(text)){
+        errorText = "Error: value should be number"
+    } else {
+        if (!checkNumberBetween(-5, -3, text)){
+            errorText = "Error: value should be between -5 and 3"
+        } else {
+            success = true
+        }
+    }
+    return [success, errorText]
+}
+function onInputTextUpdate(text){
+    let validation = checkValueWithError(text)
+    document.getElementById("input-text-error").innerHTML = validation[1];
+    document.querySelector("#button-submit").disabled = !validation[0]
+}
+
+function submitOnClick(){
+    var data = {
+        x: parseInt(document.querySelector('input[name="X"]:checked').value),
+        y: parseInt(document.querySelector('#input-y').value), 
+        R: parseInt(document.querySelector('input[name="R"]:checked').value)
+    }
+    fetch("fcgi-app", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+       console.error(error); 
+    });
+}
